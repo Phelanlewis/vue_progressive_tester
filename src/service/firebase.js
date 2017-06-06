4a518d60d1f7b8eb713a4a6f0ce98807d1b4033f
Phelan Lewis <phelanlewis@gmail.com>
2017-06-05 21:52:25 -0700

    added firebase config

---
+++ src/service/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyCpxYmACoukA1GU3thKFBcXLv6mri0qFPM",
+  authDomain: "cropchat-4ce99.firebaseapp.com",
+  databaseURL: "https://cropchat-4ce99.firebaseio.com",
+  projectId: "cropchat-4ce99",
+  storageBucket: "cropchat-4ce99.appspot.com",
+  messagingSenderId: "30640025043"
+};
+firebase.initializeApp(config);
